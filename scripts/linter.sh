21a91e143bc1683892b8eccdff6426ca3f83a6aa
Sajjad Hashemian <wolaws@gmail.com>
2018-05-27 22:10:14 +0430

    add yamllinter check

---
+++ scripts/linter.sh
@@ -0,0 +1,23 @@
+LEVEL=2
+
+if ! [ -x "$(command -v yamllinter)" ]; then
+  echo 'Error: yamllinter is not installed.' >&2
+  exit 1
+fi
+
+cd "./locale"
+
+RC=0
+
+for r in *.yml
+do
+  echo "testing $r"
+  yamllinter --file "$r" --level $LEVEL
+  if [ $? -eq 1 ]
+  then
+    RC=1
+  fi
+  echo
+done
+
+exit $RC

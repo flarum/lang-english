a2a1e8030c825af6308395bda34db0ebee4c91e2
Sajjad Hashemian <wolaws@gmail.com>
2018-05-29 15:59:39 +0430

    enhancement

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -1,5 +1,9 @@
 LEVEL=2
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 if ! [ -x "$(command -v yamllinter)" ]; then
   echo 'Error: yamllinter is not installed.' >&2
   exit 1
@@ -13,12 +17,17 @@ for r in *.yml
 do
   [ "$r" != "validation.yml" ] || continue
 
-  echo "testing $r"
+  echo "checking $r:"
+
   yamllinter --file "$r" --level $LEVEL
-  if [ $? -eq 1 ]
-  then
+
+  if [ $? -eq 1 ]; then
     RC=1
+    printf "${RED}⨉ $r faild${NC}\n"
+  else
+    printf "${GREEN}✓ $r passed${NC}\n"
   fi
+
   echo
 done
 
